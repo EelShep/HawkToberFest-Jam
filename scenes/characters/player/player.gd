1a3f7a7dce2c5cc13c02ad437812898951f76b1d
8bitprodigy <chrisxdeboy@gmail.com>
2024-09-24 16:42:12 -0700

    Got initial pass of player controller working.

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody3D
 class_name Player
 
+@export var ROT_OFFSET    : float = 0.0
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var SPEED         : float = 3.0
+@export var TURN_SPEED    : float = 2.0
+@export var JUMP_VELOCITY : float = 4.5
+
+@export var model    : Node
+@export var animator : AnimationPlayer
 
 
 func _physics_process(delta: float) -> void:
@@ -14,15 +19,21 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	var turn_dir : float = Input.get_action_strength("ui_left") - Input.get_action_strength("ui_right")
+	model.rotation.y += turn_dir * TURN_SPEED * delta
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP,model.rotation.y + ROT_OFFSET)).normalized()
 	if direction:
+		if !animator.is_playing() or animator.current_animation != "walking":
+			animator.play("walking")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if !animator.is_playing() or animator.current_animation != "idle_01":
+			animator.play("idle_01")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
